c35f5638880ce34b3606c991d0df945ac3fb8384
jtcb <jtcbrule@gmail.com>
2016-09-22 18:26:37 -0400

    Add simple_alert.py
    Example profile to send alert commands to a cellphone

---
+++ examples/simple_alert.py
@@ -0,0 +1,39 @@
+from subprocess import call
+from yuuki.dispatch import action
+
+CARRIERS = {"AT&T": "txt.att.net",
+            "T-Mobile": "tmomail.net",
+            "Verizon": "vtext.com",
+            "Sprint": "pm.sprint.com",
+            "Virgin Mobile": "vmobl.com",
+            "Tracfone": "mmst5.tracfone.com",
+            "Metro PCS": "mymetropcs.com",
+            "Boost Mobile": "myboostmobile.com",
+            "Cricket": "sms.mycricket.com",
+            "Ptel": "ptel.com",
+            "Republic Wireless": "text.republicwireless.com",
+            "Suncom": "tms.suncom.com",
+            "Ting": "message.ting.com",
+            "U.S. Cellular": "email.uscc.net",
+            "Consumer Cellular": "cingularme.com",
+            "C-Spire": "cspire1.com",
+            "Page Plus": "vtext.com"}
+
+
+@action(target='openc2:cellphone')
+def alert(target, actuator, modifier):
+    """
+    Send an alert to a cellphone.
+    
+    Required target specifiers:
+    cellphone number - target['number']
+    cell carrier - target['carrier']
+    alert message - target['message']
+    """
+    to = "{}@{}".format(target['number'], CARRIERS[target['carrier']])
+    subject = "OpenC2 Alert"
+    message = target['message']
+    mailcmd = "echo -e '{}' | mail -s '{}' {}".format(message, subject, to)
+
+    return call(mailcmd, shell=True)
+
